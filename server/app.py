ba4e700fef5c5052ac94e165e4cc691f6db08de5
Marvin-Kabugi <kabugijeff@gmail.com>
2023-06-23 16:15:05 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
@@ -14,14 +15,44 @@ app.json.compact = True
 migrate = Migrate(app, db)
 db.init_app(app)
 
+CORS(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict = [item.to_dict() for item in Plant.query.all()]
+
+        response = make_response(jsonify(plants_dict), 200)
+
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        new_record = Plant(
+            name = data.get('name'),
+            image= data.get('image'),
+            price = data.get('price'),
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
